4271bd2a8e6e5e67a79f5fcce95223ba8d55798d
Subanbek Tashbaltaev <stashbaltaev@gmail.com>
2023-01-18 19:09:53 +0600

    Init project

---
+++ src/test/java/com/booking/bookingdemo/BookingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.bookingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
